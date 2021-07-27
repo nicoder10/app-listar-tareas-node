05d42f035c9268db1de4b4f23fbe4d5cf7303a51
Nicolas Martino <martinonicolas10@gmail.com>
2021-07-27 16:54:59 -0300

    Added list getter

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const colors = require('colors');
 // const { showMenu, pause } = require('./helpers/messages');
 const { showInquirerMenu, inquirerPause, readInput } = require('./helpers/inquirer');
-//const Task = require('./models/task');
 const Tasks = require('./models/tasks');
-
 console.clear();
-/*
-let tasks = new Tasks();
-let task = new Task('Buy bread');
-tasks.list[task.id] = task;
-console.log(tasks);
-*/
+
 const main = async() => {
     let userOpt = '';
     const tasks = new Tasks();
@@ -24,7 +17,8 @@ const main = async() => {
                 tasks.createTask(desc);
                 break;
             case '2':
-                console.log(tasks._list);
+                //console.log(tasks._list);
+                console.log(tasks.listArray);
                 break;
         }
         await inquirerPause();
--- models/tasks.js
+++ models/tasks.js
@@ -10,6 +10,14 @@ class Tasks {
         const task = new Task(desc);
         this._list[task.id] = task;
     }
+    get listArray() {
+        const list = [];
+        Object.keys(this._list).forEach((key) => {
+            const task = this._list[key];
+            list.push(task);
+        });
+        return list;
+    }
 }
 
 module.exports = Tasks;
