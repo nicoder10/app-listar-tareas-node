7a87aef07db457e8977688ebf5fe067319d142d8
Nicolas Martino <martinonicolas10@gmail.com>
2021-07-27 16:44:02 -0300

    Added case 1 and 2 logic

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 const colors = require('colors');
 // const { showMenu, pause } = require('./helpers/messages');
-const { showInquirerMenu, inquirerPause } = require('./helpers/inquirer');
-const { Task } = require('./models/task');
-const { Tasks } = require('./models/tasks');
+const { showInquirerMenu, inquirerPause, readInput } = require('./helpers/inquirer');
+//const Task = require('./models/task');
+const Tasks = require('./models/tasks');
 
 console.clear();
-
+/*
 let tasks = new Tasks();
 let task = new Task('Buy bread');
 tasks.list[task.id] = task;
 console.log(tasks);
-
+*/
 const main = async() => {
-    let opt = '';
+    let userOpt = '';
+    const tasks = new Tasks();
+
     do {
-        //opt = await showMenu();
-        opt = await showInquirerMenu();
-        console.log(opt);
-        //await pause();
+        const { option } = await showInquirerMenu();
+        userOpt = option;
+        switch(option) {
+            case '1':
+                const desc = await readInput('Description: ');
+                tasks.createTask(desc);
+                break;
+            case '2':
+                console.log(tasks._list);
+                break;
+        }
         await inquirerPause();
-    } while(opt !== '0')
+    } while(userOpt !== '0')
+    
 }
 
 main(); 
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,23 +1,23 @@
 const inquirer = require('inquirer');
 
 const showInquirerMenu = async () => {
-    await inquirer
-        .prompt([
-            {
-                name: 'menu',
-                type: 'list',
-                message: 'Select an option: ',
-                choices: [
-                    `${'1'.cyan} Create task`, 
-                    `${'2'.cyan} List tasks`,
-                    `${'3'.cyan} List completed tasks`, 
-                    `${'4'.cyan} List pending tasks)`,
-                    `${'5'.cyan} Complete task(s)`, 
-                    `${'6'.cyan} Delete task`, 
-                    `${'0'.red} Exit\n`,
-                ]
-            }
-        ])
+    return await inquirer
+            .prompt([
+                {
+                    name: 'option',
+                    type: 'list',
+                    message: 'Select an option: ',
+                    choices: [
+                        { value: '1', name: `${'1'.cyan} Create task` }, 
+                        { value: '2', name: `${'2'.cyan} List tasks` },
+                        { value: '3', name: `${'3'.cyan} List completed tasks` }, 
+                        { value: '4', name: `${'4'.cyan} List pending tasks)` },
+                        { value: '5', name: `${'5'.cyan} Complete task(s)`}, 
+                        { value: '6', name: `${'6'.cyan} Delete task`}, 
+                        { value: '7', name: `${'0'.red} Exit\n` },
+                    ]
+                }
+            ])
 };
 
 const inquirerPause = async () => {
@@ -31,7 +31,27 @@ const inquirerPause = async () => {
         ])
 };
 
+const readInput = async (message) => {
+    const question = [
+      {
+        type: 'input',
+        name: 'desc',
+        message,
+        validate(value) {
+          if (value.length === 0) {
+            return 'Please enter a description';
+          }
+          return true;
+        },
+      },
+    ];
+  
+    const { desc } = await inquirer.prompt(question);
+    return desc;
+  };
+
 module.exports = {
     showInquirerMenu,
-    inquirerPause
+    inquirerPause,
+    readInput
 }
